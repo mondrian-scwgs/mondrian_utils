29331a5aa9aedd1526f41fb16f0be76b844e9a68
Diljot Grewal <diljot.grewal@gmail.com>
2022-05-16 08:30:03 -0700

    gc metrics fix

--- mondrianutils/alignment/collect_gc_metrics.py
+++ mondrianutils/alignment/collect_gc_metrics.py
@@ -23,20 +23,17 @@ def collect_gc_metrics(bias_file, output, sample_id):
     # If the file is empty (only header no data) then return 0s (dummy data)
     try:
         data = pd.read_csv(bias_file, sep='\t', skiprows=skiprows)
+        data = data[['NORMALIZED_COVERAGE', 'WINDOWS', 'GC']]
+        data = data.set_index('GC')
+        data = data.rename(columns={'NORMALIZED_COVERAGE': sample_id, 'WINDOWS': 'reference'})
+        data = data.T
+        data['cell_id'] = data.index
+        data.columns = data.columns.astype(str)
     except pd.errors.EmptyDataError:
         logging.getLogger("single_cell.align.gcbias").warn(
             'No data in the GCBias output')
         # If the file is empty (only header no data) then return 0s (dummy data)
         data = np.array([np.arange(100), [0] * 100]).T
         data = pd.DataFrame(data, columns=['gc', sample_id])
-        return data
-
-    data = data[['NORMALIZED_COVERAGE', 'WINDOWS', 'GC']]
-    data = data.set_index('GC')
-    data = data.rename(columns={'NORMALIZED_COVERAGE': sample_id, 'WINDOWS': 'reference'})
-    data = data.T
-    data['cell_id'] = data.index
-
-    data.columns = data.columns.astype(str)
 
     csverve.write_dataframe_to_csv_and_yaml(data, output, dtypes()['gc'], write_header=True)
