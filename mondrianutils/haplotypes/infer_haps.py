dcef4e036515a91e4fa9701d938d0f64c64aa183
Diljot Grewal <diljot.grewal@gmail.com>
2022-07-09 18:38:49 -0700

    fix

--- mondrianutils/haplotypes/infer_haps.py
+++ mondrianutils/haplotypes/infer_haps.py
@@ -90,8 +90,6 @@ def infer_haps(snp_genotype, chromosome, output, thousand_genomes_tar, snp_posit
 
     helpers.untar(thousand_genomes_tar, thousand_genomes_dir)
 
-    thousand_genomes_dir = os.path.join(thousand_genomes_dir, 'thousand_genomes_dir')
-
     meta = load_tar_meta(thousand_genomes_dir)
 
     if meta['ensembl_genome_version'] == 'GRCh37':
