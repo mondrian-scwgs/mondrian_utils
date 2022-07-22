5b97f0784bee50b16865a26868ba4097b4c78693
Diljot Grewal <diljot.grewal@gmail.com>
2022-07-22 11:04:39 -0700

    v0.0.54: consolidated hmmcopy script

---
+++ mondrianutils/hmmcopy/complete_hmmcopy.py
@@ -0,0 +1,111 @@
+import os
+
+import csverve.api as csverve
+import mondrianutils.helpers as helpers
+import pandas as pd
+from mondrianutils.dtypes import hmmcopy_metrics
+from mondrianutils.dtypes import hmmcopy_params
+from mondrianutils.dtypes import hmmcopy_reads
+from mondrianutils.dtypes import hmmcopy_segs
+from mondrianutils.hmmcopy.correct_read_count import CorrectReadCount
+from mondrianutils.hmmcopy.plot_hmmcopy import GenHmmPlots
+
+
+def plot_hmmcopy(
+        reads, segments, params, metrics, ref_genome, segs_out,
+        bias_out, num_states=12,
+        annotation_cols=None, sample_info=None, max_cn=None
+):
+    if not annotation_cols:
+        annotation_cols = ['cell_call', 'experimental_condition', 'sample_type',
+                           'mad_neutral_state', 'MSRSI_non_integerness',
+                           'total_mapped_reads_hmmcopy']
+
+    with GenHmmPlots(reads, segments, params, metrics, ref_genome, segs_out,
+                     bias_out, num_states=num_states,
+                     annotation_cols=annotation_cols,
+                     sample_info=sample_info, max_cn=max_cn) as plot:
+        plot.main()
+
+
+def run_hmmcopy(
+        corrected_reads,
+        tempdir,
+        metrics,
+        params,
+        reads,
+        segments,
+        output_tarball,
+        multipliers=tuple(range(1, 7)),
+        strength=1000,
+        e=0.999999,
+        mu=tuple(range(12)),
+        lambda_p=20,
+        nu=2.1,
+        kappa=(100, 100, 700, 100, 25, 25, 25, 25, 25, 25, 25, 25),
+        m=tuple(range(12)),
+        eta=50000,
+        g=3,
+        s=1
+):
+    df = pd.read_csv(corrected_reads)
+    cell_id = list(df['cell_id'].unique())
+    assert len(cell_id) == 1
+    cell_id = cell_id[0]
+
+    scripts_directory = os.path.realpath(os.path.dirname(__file__))
+    run_hmmcopy_rscript = os.path.join(scripts_directory, 'hmmcopy_single_cell.R')
+    cmd = [run_hmmcopy_rscript]
+
+    # run hmmcopy
+    cmd += ['--corrected_data=' + corrected_reads,
+            '--outdir=' + tempdir,
+            '--sample_id=' + cell_id]
+
+    cmd.append('--param_str=' + str(strength))
+    cmd.append('--param_e=' + str(e))
+    cmd.append('--param_mu=' + ','.join(map(str, mu)))
+    cmd.append('--param_l=' + str(lambda_p))
+    cmd.append('--param_nu=' + str(nu))
+    cmd.append('--param_k=' + ','.join(map(str, kappa)))
+    cmd.append('--param_m=' + ','.join(map(str, m)))
+    cmd.append('--param_eta=' + str(eta))
+    cmd.append('--param_g=' + str(g))
+    cmd.append('--param_s=' + str(s))
+    cmd.append('--param_multiplier=' + ','.join(map(str, multipliers)))
+
+    helpers.run_cmd(cmd)
+
+    csverve.rewrite_csv_file(f'{tempdir}/0/reads.csv', reads, dtypes=hmmcopy_reads.dtypes())
+    csverve.rewrite_csv_file(f'{tempdir}/0/params.csv', params, dtypes=hmmcopy_params.dtypes())
+    csverve.rewrite_csv_file(f'{tempdir}/0/segs.csv', segments, dtypes=hmmcopy_segs.dtypes())
+    csverve.rewrite_csv_file(f'{tempdir}/0/metrics.csv', metrics, dtypes=hmmcopy_metrics.dtypes())
+
+    helpers.make_tarfile(output_tarball, tempdir)
+
+
+def complete_hmmcopy(
+        readcount_wig, gc_wig_file, map_wig_file, metrics, params, reads, segments,
+        output_tarball, reference, segments_output, bias_output, cell_id, tempdir,
+        mappability_cutoff=0.9
+):
+    helpers.makedirs(tempdir)
+
+    corrected_reads = os.path.join(tempdir, 'corrected_reads.csv')
+    correction = CorrectReadCount(
+        gc_wig_file, map_wig_file, readcount_wig, corrected_reads,
+        cell_id, mappability=mappability_cutoff
+    )
+    correction.main()
+
+    hmmcopy_tempdir = os.path.join(tempdir, 'hmmcopy')
+    helpers.makedirs(hmmcopy_tempdir)
+    run_hmmcopy(
+        corrected_reads, hmmcopy_tempdir, metrics,
+        params, reads, segments, output_tarball
+    )
+
+    plot_hmmcopy(
+        reads, segments, params, metrics, reference,
+        segments_output, bias_output
+    )
--- mondrianutils/hmmcopy/utils.py
+++ mondrianutils/hmmcopy/utils.py
@@ -10,14 +10,10 @@ import pandas as pd
 import yaml
 from mondrianutils import __version__
 from mondrianutils.dtypes import hmmcopy_metrics
-from mondrianutils.dtypes import hmmcopy_params
-from mondrianutils.dtypes import hmmcopy_reads
-from mondrianutils.dtypes import hmmcopy_segs
 from mondrianutils.hmmcopy.clustering_order import add_clustering_order
-from mondrianutils.hmmcopy.correct_read_count import CorrectReadCount
+from mondrianutils.hmmcopy.complete_hmmcopy import complete_hmmcopy
 from mondrianutils.hmmcopy.generate_qc_html import generate_html_report
 from mondrianutils.hmmcopy.plot_heatmap import PlotPcolor
-from mondrianutils.hmmcopy.plot_hmmcopy import GenHmmPlots
 from mondrianutils.hmmcopy.readcounter import ReadCounter
 
 
@@ -32,79 +28,6 @@ def plot_heatmap(reads, metrics, chromosomes, output):
         chromosomes=chromosomes
     )
     plot.main()
-
-
-def plot_hmmcopy(
-        reads, segments, params, metrics, ref_genome, segs_out,
-        bias_out, num_states=12,
-        annotation_cols=None, sample_info=None, max_cn=None
-):
-    if not annotation_cols:
-        annotation_cols = ['cell_call', 'experimental_condition', 'sample_type',
-                           'mad_neutral_state', 'MSRSI_non_integerness',
-                           'total_mapped_reads_hmmcopy']
-
-    with GenHmmPlots(reads, segments, params, metrics, ref_genome, segs_out,
-                     bias_out, num_states=num_states,
-                     annotation_cols=annotation_cols,
-                     sample_info=sample_info, max_cn=max_cn) as plot:
-        plot.main()
-
-
-def run_hmmcopy(
-        corrected_reads,
-        tempdir,
-        metrics,
-        params,
-        reads,
-        segments,
-        output_tarball,
-        multipliers=tuple(range(1, 7)),
-        strength=1000,
-        e=0.999999,
-        mu=tuple(range(12)),
-        lambda_p=20,
-        nu=2.1,
-        kappa=(100, 100, 700, 100, 25, 25, 25, 25, 25, 25, 25, 25),
-        m=tuple(range(12)),
-        eta=50000,
-        g=3,
-        s=1
-):
-    df = pd.read_csv(corrected_reads)
-    cell_id = list(df['cell_id'].unique())
-    assert len(cell_id) == 1
-    cell_id = cell_id[0]
-
-    scripts_directory = os.path.realpath(os.path.dirname(__file__))
-    run_hmmcopy_rscript = os.path.join(scripts_directory, 'hmmcopy_single_cell.R')
-    cmd = [run_hmmcopy_rscript]
-
-    # run hmmcopy
-    cmd += ['--corrected_data=' + corrected_reads,
-            '--outdir=' + tempdir,
-            '--sample_id=' + cell_id]
-
-    cmd.append('--param_str=' + str(strength))
-    cmd.append('--param_e=' + str(e))
-    cmd.append('--param_mu=' + ','.join(map(str, mu)))
-    cmd.append('--param_l=' + str(lambda_p))
-    cmd.append('--param_nu=' + str(nu))
-    cmd.append('--param_k=' + ','.join(map(str, kappa)))
-    cmd.append('--param_m=' + ','.join(map(str, m)))
-    cmd.append('--param_eta=' + str(eta))
-    cmd.append('--param_g=' + str(g))
-    cmd.append('--param_s=' + str(s))
-    cmd.append('--param_multiplier=' + ','.join(map(str, multipliers)))
-
-    helpers.run_cmd(cmd)
-
-    csverve.rewrite_csv_file('output/0/reads.csv', reads, dtypes=hmmcopy_reads.dtypes())
-    csverve.rewrite_csv_file('output/0/params.csv', params, dtypes=hmmcopy_params.dtypes())
-    csverve.rewrite_csv_file('output/0/segs.csv', segments, dtypes=hmmcopy_segs.dtypes())
-    csverve.rewrite_csv_file('output/0/metrics.csv', metrics, dtypes=hmmcopy_metrics.dtypes())
-
-    helpers.make_tarfile(output_tarball, tempdir)
 
 
 def add_mappability(reads, annotated_reads):
@@ -292,78 +215,25 @@ def parse_args():
         help='regions to skip'
     )
 
-    correct_readcount = subparsers.add_parser('correct_readcount')
-    correct_readcount.set_defaults(which='correct_readcount')
-    correct_readcount.add_argument(
-        '--infile'
-    )
-    correct_readcount.add_argument(
-        '--gc_wig_file'
-    )
-    correct_readcount.add_argument(
-        '--map_wig_file'
-    )
-    correct_readcount.add_argument(
-        '--outfile'
-    )
-    correct_readcount.add_argument(
-        '--cell_id'
-    )
-    correct_readcount.add_argument(
+    run_hmmcopy = subparsers.add_parser('hmmcopy')
+    run_hmmcopy.set_defaults(which='hmmcopy')
+    run_hmmcopy.add_argument('--readcount_wig', required=True)
+    run_hmmcopy.add_argument('--gc_wig_file', required=True)
+    run_hmmcopy.add_argument('--map_wig_file', required=True)
+    run_hmmcopy.add_argument('--metrics', required=True)
+    run_hmmcopy.add_argument('--params', required=True)
+    run_hmmcopy.add_argument('--reads', required=True)
+    run_hmmcopy.add_argument('--segments', required=True)
+    run_hmmcopy.add_argument('--output_tarball', required=True)
+    run_hmmcopy.add_argument('--reference', required=True)
+    run_hmmcopy.add_argument('--segments_output', required=True)
+    run_hmmcopy.add_argument('--bias_output', required=True)
+    run_hmmcopy.add_argument('--cell_id', required=True)
+    run_hmmcopy.add_argument('--tempdir', required=True)
+    run_hmmcopy.add_argument(
         '--map_cutoff',
         default=0.9,
         type=float,
-    )
-
-    plot_hmmcopy = subparsers.add_parser('plot_hmmcopy')
-    plot_hmmcopy.set_defaults(which='plot_hmmcopy')
-    plot_hmmcopy.add_argument(
-        '--reads'
-    )
-    plot_hmmcopy.add_argument(
-        '--segments'
-    )
-    plot_hmmcopy.add_argument(
-        '--params'
-    )
-    plot_hmmcopy.add_argument(
-        '--metrics'
-    )
-    plot_hmmcopy.add_argument(
-        '--reference'
-    )
-    plot_hmmcopy.add_argument(
-        '--segments_output'
-    )
-    plot_hmmcopy.add_argument(
-        '--bias_output'
-    )
-    plot_hmmcopy.add_argument(
-        '--cell_id'
-    )
-
-    run_hmmcopy = subparsers.add_parser('run_hmmcopy')
-    run_hmmcopy.set_defaults(which='run_hmmcopy')
-    run_hmmcopy.add_argument(
-        '--corrected_reads'
-    )
-    run_hmmcopy.add_argument(
-        '--tempdir'
-    )
-    run_hmmcopy.add_argument(
-        '--metrics'
-    )
-    run_hmmcopy.add_argument(
-        '--params'
-    )
-    run_hmmcopy.add_argument(
-        '--reads'
-    )
-    run_hmmcopy.add_argument(
-        '--segments'
-    )
-    run_hmmcopy.add_argument(
-        '--output_tarball'
     )
 
     add_mappability = subparsers.add_parser('add_mappability')
@@ -504,22 +374,14 @@ def utils():
                          args['chromosomes'], args['mapping_quality_threshold'],
                          excluded=args['exclude_list']) as rcount:
             rcount.main()
-    elif args['which'] == 'correct_readcount':
-        CorrectReadCount(args["gc_wig_file"],
-                         args['map_wig_file'],
-                         args['infile'],
-                         args['outfile'],
-                         args['cell_id'],
-                         mappability=args['map_cutoff']).main()
-    elif args['which'] == 'plot_hmmcopy':
-        plot_hmmcopy(
-            args['reads'], args['segments'], args['params'], args['metrics'],
-            args['reference'], args['segments_output'], args['bias_output']
-        )
-    elif args['which'] == 'run_hmmcopy':
-        run_hmmcopy(
-            args['corrected_reads'], args['tempdir'], args['metrics'], args['params'],
-            args['reads'], args['segments'], args['output_tarball']
+    elif args['which'] == 'hmmcopy':
+        complete_hmmcopy(
+            args['readcount_wig'], args["gc_wig_file"], args['map_wig_file'],
+            args['metrics'], args['params'],
+            args['reads'], args['segments'], args['output_tarball'],
+            args['reference'], args['segments_output'], args['bias_output'],
+            args['cell_id'], args['tempdir'],
+            mappability_cutoff=args['map_cutoff']
         )
     elif args['which'] == 'add_mappability':
         add_mappability(args['infile'], args['outfile'])
