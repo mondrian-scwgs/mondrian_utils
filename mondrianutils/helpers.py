99818af1aa809be662977aabfb0c44ab760fa8bc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 23:09:12 +0000

    Adding tarfile member sanitization to extractall()

--- mondrianutils/helpers.py
+++ mondrianutils/helpers.py
@@ -163,7 +163,26 @@ def merge_bams(infiles, outfile, tempdir, ncores):
 def untar(input_tar, outdir):
     makedirs(outdir)
     with tarfile.open(input_tar) as tar:
-        tar.extractall(path=outdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, path=outdir)
 
 
 def metadata_helper(files_json, metadata_yamls, samples, wf_type):
