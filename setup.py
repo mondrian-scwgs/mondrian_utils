d151ef575e4975d0dd5c4f3724f36a04d5a1ee9b
Diljot Grewal <diljot.grewal@gmail.com>
2023-12-15 10:56:54 -0800

    v0.0.91: replacing argparse with clock

--- setup.py
+++ setup.py
@@ -12,21 +12,21 @@ setup(
     entry_points={
         'console_scripts': [
             'mondrianutils = mondrianutils.run:main',
-            'variant_utils = mondrianutils.variant_calling.utils:utils',
+            'variant_utils = mondrianutils.variant_calling.utils:cli',
             'breakpoint_utils = mondrianutils.breakpoint_calling.utils:cli',
-            'alignment_utils = mondrianutils.alignment.utils:utils',
-            'hmmcopy_utils = mondrianutils.hmmcopy.utils:utils',
-            'csverve_utils = mondrianutils.io.csverve:utils',
-            'pdf_utils = mondrianutils.io.pdf:utils',
-            'vcf_utils = mondrianutils.io.vcf:utils',
-            'bam_utils = mondrianutils.io.bam:utils',
+            'alignment_utils = mondrianutils.alignment.utils:cli',
+            'hmmcopy_utils = mondrianutils.hmmcopy.utils:cli',
+            'csverve_utils = mondrianutils.io.csverve:cli',
+            'pdf_utils = mondrianutils.io.pdf:cli',
+            'vcf_utils = mondrianutils.io.vcf:cli',
+            'bam_utils = mondrianutils.io.bam:cli',
             'haplotype_utils = mondrianutils.haplotypes.utils:cli',
-            'snv_genotyping_utils = mondrianutils.snv_genotyping.utils:utils',
-            'sv_genotyping_utils = mondrianutils.sv_genotyping.utils:utils',
-            'reference_utils = mondrianutils.reference.utils:utils',
-            'dlp_utils = mondrianutils.dlp_utils.utils:utils',
-            'normalizer_utils = mondrianutils.normalizer.utils:utils',
-            'mondrian_build_utils = mondrianutils.mondrian_build.utils:utils'
+            'snv_genotyping_utils = mondrianutils.snv_genotyping.utils:cli',
+            'sv_genotyping_utils = mondrianutils.sv_genotyping.utils:cli',
+            'reference_utils = mondrianutils.reference.utils:cli',
+            'dlp_utils = mondrianutils.dlp_utils.utils:cli',
+            'normalizer_utils = mondrianutils.normalizer.utils:cli',
+            'mondrian_build_utils = mondrianutils.mondrian_build.utils:cli'
         ]
     },
     package_data={'': ['*.py', '*.R', '*.npz', "*.yaml", "data/*", "*.sh"]}
