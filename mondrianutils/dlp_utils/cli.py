a93044a92e745819d2d2bf6f0a3b481d4c7b150a
Diljot Grewal <diljot.grewal@gmail.com>
2023-12-18 11:14:15 -0800

    v0.0.91: replacing argparse with clock

--- mondrianutils/dlp_utils/cli.py
+++ mondrianutils/dlp_utils/cli.py
@@ -1,6 +1,5 @@
 import click
-
-from mondrianutils.dlp_utils.dlp_bams_to_mondrian_bam import dlp_bams_to_mondrian_bam
+import mondrianutils
 
 
 @click.group()
@@ -12,8 +11,8 @@ def cli():
 @click.option('--output', required=True)
 @click.option('--tempdir', required=True)
 @click.option('--cores', default=8)
-def dlp_bams_to_mondrian_bam_cmd(dlp_bam_dir, output, tempdir, cores):
-    dlp_bams_to_mondrian_bam(dlp_bam_dir, output, tempdir, ncores=cores)
+def dlp_bams_to_mondrian_bam(dlp_bam_dir, output, tempdir, cores):
+    mondrianutils.dlp_utils.dlp_bams_to_mondrian_bam(dlp_bam_dir, output, tempdir, ncores=cores)
 
 if __name__ == '__main__':
     cli()
