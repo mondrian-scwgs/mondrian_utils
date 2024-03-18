684185f75962b82d52b6ee38ce7afe84b7deb913
Diljot Grewal <diljot.grewal@gmail.com>
2024-03-18 07:28:03 -0700

    command to split vcf by numlines, fixed metadata for infer and count haps

--- mondrianutils/io/__init__.py
+++ mondrianutils/io/__init__.py
@@ -5,7 +5,8 @@ from .csverve import rewrite_csv
 from .pdf import merge_pdfs
 from .pdf import merge_pdfs_with_scaling
 
-from .vcf import split_vcf
+from .vcf import split_vcf_by_lines
+from .vcf import split_vcf_into_numsplits
 from .vcf import split_vcf_by_chrom
 from .vcf import remove_duplicates
 from .vcf import exclude_blacklist
