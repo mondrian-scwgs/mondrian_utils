4ad27b44724b4666936efa0b07c9ed09a2954b75
Andrew McPherson <andrew.mcpherson@gmail.com>
2025-03-28 12:26:07 -0400

    refactor readcounter to output dataframe and allow more options

--- mondrianutils/hmmcopy/readcounter.py
+++ mondrianutils/hmmcopy/readcounter.py
@@ -11,175 +11,57 @@ import argparse
 import numpy as np
 import pandas as pd
 import pysam
+import tqdm
 
 
-class ReadCounter(object):
+def create_bincounts():
+    return defaultdict(int)
+
+
+def create_cellcounts():
+    return defaultdict(create_bincounts)
+
+
+class CellReadCounts:
     """
-    calculate reads per bin from the input bam file
+    Store and manage read counts per bin for each cell and chromosome.
+
+    :param chromosomes: List of chromosome names
+    :type chromosomes: list of str
+    :param chr_lengths: Chromosome lengths keyed by chromosome name
+    :type chr_lengths: dict
+    :param window_size: Bin size used to count reads
+    :type window_size: int
     """
-
-    def __init__(
-            self, bam, output, window_size, chromosomes, mapq,
-            seg=None, excluded=None, reference=None
-    ):
-        self.bamfile = bam
-
-        self.output = output
-
+    
+    def __init__(self, chromosomes, chr_lengths, window_size):
+        self.chromosomes = chromosomes
+        self.chr_lengths = chr_lengths
         self.window_size = window_size
+        self.counts = defaultdict(create_cellcounts)
 
-        if chromosomes:
-            self.chromosomes = chromosomes
-        else:
-            self.chromosomes = self.__get_chr_names()
-
-        self.mapq_threshold = mapq
-
-        self.seg = seg
-
-        if excluded is not None:
-            self.excluded = pd.read_csv(excluded, sep="\t", )
-            self.excluded.columns = ["chrom", "start", "end"]
-        else:
-            self.excluded = None
-
-        if self.is_empty():
-            self.bam = None
-            self.chr_lengths = None
-            self.cells = []
-        else:
-            self.bam = self.__get_bam_reader()
-            self.chr_lengths = self.__get_chr_lengths()
-            self.cells = self.get_cells()
-
-    def is_empty(self):
-        if os.path.getsize(self.bamfile) < 10:
-            with open(self.bamfile, 'rt') as reader:
-                if reader.readline().startswith('NO DATA'):
-                    return True
-
-    def __get_bam_header(self):
-        return self.bam.header
-
-    def get_cells(self):
-        header = self.__get_bam_header()
-        if isinstance(header, dict):
-            comments = [v.replace('CB:', '') for v in header['CO']]
-            return comments
-
-        cells = []
-        for line in str(header).split('\n'):
-            if not line.startswith("@CO"):
-                continue
-            line = line.strip().split()
-            cb = line[1]
-            cell = cb.split(':')[1]
-            cells.append(cell)
-
-        print('cells', cells)
-        return cells
-
-    def __get_chrom_excluded(self, chrom):
-        # chrom_excluded = np.zeros(chrom_length, dtype=np.uint8)
-        # add some padding in case the list is 1 based and chr length is 0 based
-
-        chrom_length = self.chr_lengths[chrom]
-        chrom_excluded = np.zeros(chrom_length + 1, dtype=np.uint8)
-
-        for start, end in self.excluded.loc[self.excluded['chrom'] == chrom, ['start', 'end']].values:
-            start = min(start, chrom_length)
-            end = min(end, chrom_length)
-            chrom_excluded[start:end] = 1
-
-        return chrom_excluded
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-        # clean up output if there are any exceptions
-        # if exc_type and os.path.exists(self.output):
-        #     os.remove(self.output)
-
-    def __get_chr_lengths(self):
-        """ returns dict with chromosome names and lengths
-        :returns dictionary with chromosome name (str) and lengths(int)
-        :rtype dictionary
+    def get_observed_cells(self):
         """
+        Get a sorted list of all observed cell IDs.
 
-        names = self.bam.references
-        lengths = self.bam.lengths
-        return {name: length for name, length in zip(names, lengths)}
-
-    def __get_bam_reader(self):
-        """returns pysam bam object
-        :returns pysam bam object
+        :return: List of cell IDs
+        :rtype: list of str
         """
-        return pysam.AlignmentFile(self.bamfile, 'rb')
-
-    def __get_chr_names(self):
-        """extracts chromosome names from the bam file
-        :returns list of chromosome names
-        :rtype list
-        """
-        return self.bam.references
-
-    def __fetch(self, chrom, start, end):
-        """returns iterator over reads in the specified region
-        :param chrom: chromosome name (str)
-        :param start: bin starting pos (int)
-        :param end: bin end pos (int)
-        :returns iterator over reads
-        """
-        return self.bam.fetch(chrom, start, end)
-
-    def filter(self, pileupobj, chrom_excluded=None):
-        """remove low mapping quality reads and duplicates
-        :param pileupobj: pysam read object
-        :returns boolean: false if the read passes filters.
-        :rtype boolean
-        """
-
-        pos = pileupobj.reference_start
-        if chrom_excluded is not None and chrom_excluded[pos]:
-            return True
-
-        if pileupobj.is_duplicate:
-            return True
-
-        if pileupobj.mapping_quality < self.mapq_threshold:
-            return True
-
-        return False
-
-    def add_track_header(self, outfile, cell_id):
-        outstr = "track type=wiggle_0 name={}\n".format(cell_id)
-        outfile.write(outstr)
-
-    def write_header(self, chrom, outfile, add_track=False, cell_id=None):
-        """writes headers, single header if seg format,
-        one header per chromosome otherwise.
-        :param chrom: chromosome name
-        :param outfile: output file object.
-        """
-
-        outstr = "fixedStep chrom=%s start=1 step=%s span=%s\n" \
-                 % (chrom, self.window_size, self.window_size)
-        outfile.write(outstr)
-
-    def write(self, count, outfile):
-        """writes bin and counts to the output file.
-        supports seg and wig formats
-        :param chrom: chromosome name
-        :param start: bin start
-        :param stop: bin stop
-        :param count: no of reads in the bin
-        :param outfile: output file object.
-        """
-        outfile.write(str(count) + '\n')
+        cells = set()
+        for chrom, data in self.counts.items():
+            for cell in data:
+                cells.add(cell)
+        return list(sorted(cells))
 
     def get_all_bins(self, chrom):
+        """
+        Get all bins for a given chromosome.
+
+        :param chrom: Chromosome name
+        :type chrom: str
+        :return: List of bin tuples (start, end)
+        :rtype: list of tuple
+        """
         reflen = self.chr_lengths[chrom]
 
         start = 0
@@ -191,7 +73,82 @@ class ReadCounter(object):
             bins.append((start, end))
         return bins
 
+    def write_wig_files(self, output_dir, cells=None):
+        """
+        Write wiggle format output files for each cell.
+
+        :param output_dir: Directory to write .wig files
+        :type output_dir: str
+        :param cells: Optional list of cell IDs to write; all if None
+        :type cells: list of str or None
+        """
+        if cells is None:
+            cells = self.get_observed_cells()
+
+        for cell in cells:
+            with open(os.path.join(output_dir, '{}.wig'.format(cell)), 'wt') as outfile:
+
+                header_str = "track type=wiggle_0 name={}\n".format(cell)
+                outfile.write(header_str)
+
+                for chrom in self.chromosomes:
+                    track_header_str = "fixedStep chrom={} start=1 step={} span={}\n".format(
+                        chrom, self.window_size, self.window_size)
+                    outfile.write(track_header_str)
+
+                    bins = self.get_all_bins(chrom)
+                    for bin in bins:
+                        outfile.write(str(self.counts[chrom][cell][bin]) + '\n')
+
+    def write_dataframe(self, output_filename, cells=None):
+        """
+        Generate and write read counts as a pandas DataFrame.
+
+        :param output_filename: Output file path for the dataframe
+        :type output_filename: str
+        :param cells: Optional list of cell IDs to include
+        :type cells: list of str or None
+        """
+        if cells is None:
+            cells = self.get_observed_cells()
+
+        data = []
+        for cell in cells:
+            cell_data = []
+            for chrom in self.chromosomes:
+                bins = self.get_all_bins(chrom)
+                for bin in bins:
+                    start, end = bin
+                    cell_data.append({
+                        'cell_id': cell,
+                        'chromosome': chrom,
+                        'start': start,
+                        'end': end,
+                        'count': self.counts[chrom][cell][bin],
+                    })
+            
+            cell_data = pd.DataFrame(cell_data)
+            cell_data['cell_id'] = pd.Categorical(cell_data['cell_id'], categories=cells)
+            cell_data['chromosome'] = pd.Categorical(cell_data['chromosome'], categories=self.chromosomes)
+
+            data.append(cell_data)
+
+        data = pd.concat(data, ignore_index=True)
+
+        data.to_csv(output_filename, index=False)
+
     def get_overlapping_bin(self, pos, chrom):
+        """
+        Get the bin (start, end) that overlaps with a given position.
+
+        :param pos: Genomic position
+        :type pos: int
+        :param chrom: Chromosome name
+        :type chrom: str
+        :return: Bin as (start, end)
+        :rtype: tuple of int
+        """
+
         ## TODO: refactor.
         # if pos = 100 and binsize is 10. put it in 90,100. not 100,110
         if pos > self.window_size and pos % self.window_size == 0:
@@ -206,63 +163,186 @@ class ReadCounter(object):
 
         return (start, end)
 
-    def get_data(self, chrom):
-        """iterates over reads, calculates counts and writes to output
-        :param data: pysam iterator over reads
-        :param chrom: str: chromosome name
-        :param outfile: output file object
+    def add_read(self, chrom, cell_id, pos):
         """
+        Add a read to the count for a given cell, chromosome, and position.
 
-        data = {cell: defaultdict(int) for cell in self.cells}
-
-        bins = self.get_all_bins(chrom)
-        bins = set(bins)
-
-        chrom_excluded = None
-        if self.excluded is not None:
-            chrom_excluded = self.__get_chrom_excluded(chrom)
-
-        for pileupobj in self.__fetch(chrom, 0, self.chr_lengths[chrom]):
-            if self.filter(pileupobj, chrom_excluded):
-                continue
-
-            cell_id = pileupobj.get_tag('CB')
-
-            binval = self.get_overlapping_bin(pileupobj.pos, chrom)
-            assert binval in bins, (binval, bins)
-
-            data[cell_id][binval] += 1
-        return data
-
-    def main(self):
-        """for each chromosome, iterate over all reads. use starting position
-        of the read to calculate read counts per bin (no double counting).
+        :param chrom: Chromosome name
+        :type chrom: str
+        :param cell_id: Cell barcode/tag
+        :type cell_id: str
+        :param pos: Genomic position of the read
+        :type pos: int
         """
-        if os.path.exists(self.output):
-            files = glob.glob('{}/*'.format(self.output))
-            for f in files:
-                os.remove(f)
+        binval = self.get_overlapping_bin(pos, chrom)
+        self.counts[chrom][cell_id][binval] += 1
+
+
+def merge_cell_read_counts(counts_list, chromosomes):
+    """
+    Merge read count data from multiple CellReadCounts objects.
+
+    :param counts_list: List of CellReadCounts instances
+    :type counts_list: list of CellReadCounts
+    :param chromosomes: Chromosomes to include in merged data
+    :type chromosomes: list of str
+    :return: Merged CellReadCounts object
+    :rtype: CellReadCounts
+    """
+    assert len(counts_list) > 0
+
+    window_size = counts_list[0].window_size
+    chr_lengths = counts_list[0].chr_lengths
+    for counts in counts_list:
+        assert window_size == counts.window_size
+        assert set(chr_lengths.items()) == set(counts.chr_lengths.items())
+        assert set(counts.chromosomes).issubset(chromosomes)
+
+    merged = CellReadCounts(chromosomes, chr_lengths, window_size)
+    for counts in counts_list:
+        for chrom, chrom_data in sorted(counts.counts.items()):
+            for cell, cell_data in chrom_data.items():
+                for bin, count in cell_data.items():
+                    merged.counts[chrom][cell][bin] += count
+    
+    return merged
+
+
+class ReadCounter:
+    """
+    Count reads per bin from a BAM file, optionally filtering by mapping quality and exclusion regions.
+
+    :param bam: Path to input BAM file
+    :type bam: str
+    :param window_size: Bin size to use for counting
+    :type window_size: int
+    :param mapq: Mapping quality threshold
+    :type mapq: int
+    :param excluded: Optional path to regions to exclude
+    :type excluded: str or None
+    :param tag_name: Read tag to use for cell barcodes (default: 'CB')
+    :type tag_name: str
+    :param ignore_missing_tags: Skip reads without the barcode tag
+    :type ignore_missing_tags: bool
+    """
+
+    def __init__(
+            self, bam, window_size, mapq, excluded=None, tag_name='CB',
+            ignore_missing_tags=False,
+    ):
+        self.bamfile = bam
+        self.window_size = window_size
+        self.mapq_threshold = mapq
+
+        if excluded is not None:
+            self.excluded = pd.read_csv(excluded, sep="\t", )
+            self.excluded.columns = ["chrom", "start", "end"]
         else:
-            os.makedirs(self.output)
+            self.excluded = None
+        
+        self.tag_name = tag_name
+        self.ignore_missing_tags = ignore_missing_tags
 
         if self.is_empty():
-            return
+            self.chr_lengths = None
+        else:
+            self.chr_lengths = self.__get_chr_lengths()
 
-        add_track = True
-        for chrom in self.chromosomes:
+    def is_empty(self):
+        """
+        Check if the BAM file is empty or contains 'NO DATA'.
 
-            data = self.get_data(chrom)
+        :return: True if file is empty or contains 'NO DATA'
+        :rtype: bool
+        """
+        if os.path.getsize(self.bamfile) < 10:
+            with open(self.bamfile, 'rt') as reader:
+                if reader.readline().startswith('NO DATA'):
+                    return True
 
-            bins = self.get_all_bins(chrom)
+    def __get_chrom_excluded(self, chrom):
+        """
+        Generate a binary mask of excluded positions for a chromosome.
 
-            for cell in data:
-                with open(os.path.join(self.output, '{}.wig'.format(cell)), 'at') as outfile:
-                    if add_track:
-                        self.add_track_header(outfile, cell)
-                    self.write_header(chrom, outfile)
-                    for binval in bins:
-                        self.write(data[cell][binval], outfile)
-            add_track = False
+        :param chrom: Chromosome name
+        :type chrom: str
+        :return: Numpy array where 1 indicates excluded position
+        :rtype: numpy.ndarray
+        """
+        chrom_length = self.chr_lengths[chrom]
+        chrom_excluded = np.zeros(chrom_length + 1, dtype=np.uint8)
+
+        for start, end in self.excluded.loc[self.excluded['chrom'] == chrom, ['start', 'end']].values:
+            start = min(start, chrom_length)
+            end = min(end, chrom_length)
+            chrom_excluded[start:end] = 1
+
+        return chrom_excluded
+
+    def __get_chr_lengths(self):
+        """
+        Get chromosome lengths from BAM file header.
+
+        :return: Dictionary with chromosome names and lengths
+        :rtype: dict
+        """
+        with pysam.AlignmentFile(self.bamfile, 'rb') as bam:
+            names = bam.references
+            lengths = bam.lengths
+            return {name: length for name, length in zip(names, lengths)}
+
+    def filter(self, pileupobj, chrom_excluded=None):
+        """
+        Apply filtering to a read: exclude low mapping quality, duplicates, and masked regions.
+
+        :param pileupobj: Pysam read object
+        :type pileupobj: pysam.AlignedSegment
+        :param chrom_excluded: Optional binary mask of excluded positions
+        :type chrom_excluded: numpy.ndarray or None
+        :return: True if the read should be excluded
+        :rtype: bool
+        """
+        pos = pileupobj.reference_start
+        if chrom_excluded is not None and chrom_excluded[pos]:
+            return True
+
+        if pileupobj.is_duplicate:
+            return True
+
+        if pileupobj.mapping_quality < self.mapq_threshold:
+            return True
+
+        return False
+
+    def get_data(self, chrom):
+        """
+        Iterate over reads in a chromosome, count reads per bin.
+
+        :param chrom: Chromosome name
+        :type chrom: str
+        :return: CellReadCounts object with counts for the chromosome
+        :rtype: CellReadCounts
+        """
+        with pysam.AlignmentFile(self.bamfile, 'rb') as bam:
+
+            counts = CellReadCounts([chrom], self.chr_lengths, self.window_size)
+
+            chrom_excluded = None
+            if self.excluded is not None:
+                chrom_excluded = self.__get_chrom_excluded(chrom)
+
+            for pileupobj in tqdm.tqdm(bam.fetch(chrom)):
+                if self.filter(pileupobj, chrom_excluded):
+                    continue
+
+                if self.ignore_missing_tags and not pileupobj.has_tag(self.tag_name):
+                    continue
+
+                cell_id = pileupobj.get_tag(self.tag_name)
+
+                counts.add_read(chrom, cell_id, pileupobj.pos)
+            
+            return counts
 
 
 def parse_args():
